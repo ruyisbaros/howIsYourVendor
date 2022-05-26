8eb6e789ba7932bf0c843042687be6503f984faa
ahmet <ahmet.erdonmez77@gmail.com>
2022-05-26 22:24:43 +0200

    get single profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Info from '../components/profile/Info'
+import Posts from '../components/profile/Posts'
+import { useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux';
 
 const Profile = () => {
+
+    const { profile } = useSelector(store => store.profile)
+    const { id } = useParams()
+    //console.log(id);
+
     return (
-        <div>Profile</div>
+        <div className="profile">
+            <Info />
+            <Posts />
+        </div>
     )
 }
 
