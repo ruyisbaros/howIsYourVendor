f5c3b0f9fce05e49f4c3fe473e92b5170044007e
ahmet <ahmet.erdonmez77@gmail.com>
2022-05-26 18:14:53 +0200

    user controllers begin

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,2 +1,4 @@
 const User = require("../models/userModel")
 const asyncHandler = require("express-async-handler")
+
+exports.searchUser = asyncHandler(async (req, res) => { })
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,3 +1,7 @@
 const router = require('express').Router()
+const { searchUser } = require('../controllers/userControllers')
+const { protect } = require('../middleWares/authMiddleWare')
+
+router.get('/', protect, searchUser)
 
 module.exports = router
